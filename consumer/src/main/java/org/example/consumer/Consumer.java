eefa12297643857f0b75339a9b6da283f66b558f
Anton <lindskog96@live.se>
2023-04-25 16:40:36 +0200

    updated.

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -7,28 +7,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-import java.util.Scanner;
-import java.util.ServiceLoader;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class Consumer {
     private static final Scanner scanner = new Scanner(System.in);
-    private static String input;
+
+    private static int input;
+
 
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, IOException, NoSuchMethodException, InstantiationException {
 
+
+        Scanner scanner = new Scanner(System.in);
+        String inputs = scanner.nextLine();
         ServiceLoader<Greeting> greetings = ServiceLoader.load(Greeting.class);
 
-        for ( var greeting : greetings) {
-            System.out.println(greeting.nationalGreet());
-        }
+        Map<String, Greeting> loaderMap = new HashMap<>();
+        greetingMapping(loaderMap, greetings);
+        System.out.println(loaderMap.get(inputs).nationalGreet());
+            //  System.out.println(greeting.nationalGreet());
 
+/*
+        System.out.println(loaderMap.keySet());
 
         Set<Class> classes = findAllClasses("org.example.provider");
-
         for (var c : classes) {
             var annotation = (Language) c.getAnnotation(Language.class);
             if (annotation != null) {
@@ -39,13 +43,33 @@ public class Consumer {
                     if (m.getReturnType().equals(String.class) && m.getParameterCount() == 0) {
                         var s = m.invoke(o);
                         if (s instanceof String string)
-                            System.out.println(string);
+                        System.out.println(string);
                     }
                 }
 
             }
         }
+            private static Set<Class> findAllClasses(String packageName) throws IOException {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        return reader.lines()
+                .filter(line -> line.endsWith(".class"))
+                .map(line -> getClass(line, packageName))
+                .collect(Collectors.toSet());
     }
+    }*/
+    }
+
+    private static void greetingMapping(Map<String, Greeting> loaderMap, ServiceLoader<Greeting> greetings) {
+        for(Greeting service : greetings){
+            Language annotation = service.getClass().getAnnotation(Language.class);
+            if(annotation != null) {
+                loaderMap.put(annotation.value(), service);
+            }
+        }
+    }
+
 
     private static Set<Class> findAllClasses(String packageName) throws IOException {
         InputStream stream = ClassLoader.getSystemClassLoader()
@@ -62,23 +86,13 @@ public class Consumer {
             return Class.forName(packageName + "."
                     + className.substring(0, className.lastIndexOf('.')));
         } catch (ClassNotFoundException e) {
-            // handle the exception
         }
         return null;
     }
+
 }
 
 
-/*        private static List<Greeting> get(String annotationType){
-            return ServiceLoader.load(Greeting.class)
-                    .stream()
-                    .filter(c -> c.type().isAnnotationPresent(Language.class)
-                            && c.type().getAnnotation(Language.class)
-                            .value()
-                            .equals(annotationType))
-                    .map(ServiceLoader.Provider::get)
-                    .toList();
-        }*/
 
 
 
