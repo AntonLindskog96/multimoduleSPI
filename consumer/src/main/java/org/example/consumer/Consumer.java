76caba7452852550d57433ac3f7e87d2c1a50fc2
Anton <lindskog96@live.se>
2023-04-26 09:13:22 +0200

    updated.

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -1,98 +1,47 @@
 package org.example.consumer;
 import org.example.service.Greeting;
 import org.example.service.annotation.Language;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.util.*;
-import java.util.stream.Collectors;
-
 
 public class Consumer {
-    private static final Scanner scanner = new Scanner(System.in);
-
-    private static int input;
-
-
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, IOException, NoSuchMethodException, InstantiationException {
-
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String inputs = scanner.nextLine();
         ServiceLoader<Greeting> greetings = ServiceLoader.load(Greeting.class);
 
         Map<String, Greeting> loaderMap = new HashMap<>();
         greetingMapping(loaderMap, greetings);
-        System.out.println(loaderMap.get(inputs).nationalGreet());
-            //  System.out.println(greeting.nationalGreet());
 
-/*
-        System.out.println(loaderMap.keySet());
+        String inputs;
+        while (true) {
+            loaderMap.keySet().forEach(System.out::println);
+            System.out.println("Enter a language code ('exit' to quit): ");
+            inputs = scanner.nextLine();
 
-        Set<Class> classes = findAllClasses("org.example.provider");
-        for (var c : classes) {
-            var annotation = (Language) c.getAnnotation(Language.class);
-            if (annotation != null) {
-                System.out.println(annotation.value());
-                var o = c.getConstructor().newInstance();
-                var methods = c.getMethods();
-                for (var m : methods) {
-                    if (m.getReturnType().equals(String.class) && m.getParameterCount() == 0) {
-                        var s = m.invoke(o);
-                        if (s instanceof String string)
-                        System.out.println(string);
-                    }
-                }
-
+            if (inputs.equalsIgnoreCase("exit")) {
+                break;
             }
+
+            Greeting greeting = loaderMap.get(inputs);
+            if (greeting == null) {
+                System.out.println("Unknown language code.");
+                continue;
+            }
+
+            System.out.println(greeting.nationalGreet());
+            System.out.println("-----------------------------------");
         }
-            private static Set<Class> findAllClasses(String packageName) throws IOException {
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        return reader.lines()
-                .filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, packageName))
-                .collect(Collectors.toSet());
-    }
-    }*/
     }
 
     private static void greetingMapping(Map<String, Greeting> loaderMap, ServiceLoader<Greeting> greetings) {
-        for(Greeting service : greetings){
+        for (Greeting service : greetings) {
             Language annotation = service.getClass().getAnnotation(Language.class);
-            if(annotation != null) {
+            if (annotation != null) {
                 loaderMap.put(annotation.value(), service);
             }
         }
     }
 
-
-    private static Set<Class> findAllClasses(String packageName) throws IOException {
-        InputStream stream = ClassLoader.getSystemClassLoader()
-                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        return reader.lines()
-                .filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, packageName))
-                .collect(Collectors.toSet());
-    }
-
-    private static Class getClass(String className, String packageName) {
-        try {
-            return Class.forName(packageName + "."
-                    + className.substring(0, className.lastIndexOf('.')));
-        } catch (ClassNotFoundException e) {
-        }
-        return null;
-    }
-
 }
-
-
 
 
 
--- provider/src/main/java/org/example/provider/AnnotatedGreeting.java
+++
@@ -1,6 +0,0 @@
-package org.example.provider;
-
-import org.example.service.annotation.Language;
-
-public class AnnotatedGreeting {
-}
